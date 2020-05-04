52f802f5f6d83157757540d928f67490ac9179c4
janelle mellor <janellemellor@gmail.com>
2020-05-04 16:35:21 -0700

    add fetch call to handle submit

--- src/containers/ArtistSearch/ArtistSearch.jsx
+++ src/containers/ArtistSearch/ArtistSearch.jsx
@@ -1,15 +1,19 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Search from '../../components/Search/Search.jsx';
+import { fetchArtists } from '../../services/getArtistData.js';
 
 const ArtistSearch = () => {
   const [artistNameInput, setArtistNameInput] = useState('');
-  const [findArtist, setFindArtist] = useState('');
+  const [artistName, setArtistName] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    fetchArtists(artistName)
+      .then(res => setArtistName(res));
+    console.log(artistName);
+  }; 
 
-  };
   const handleChange = ({ target }) => setArtistNameInput(target.value);
    
   return (
