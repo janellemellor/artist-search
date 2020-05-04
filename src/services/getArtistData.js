5e6db6c3a61291c613fa1aa933f84b341d26da11
janelle mellor <janellemellor@gmail.com>
2020-05-04 16:26:04 -0700

    add service function to get artist by name

---
+++ src/services/getArtistData.js
@@ -0,0 +1,8 @@
+export const fetchArtists = (artistName) => {
+  return fetch(`http://musicbrainz.org/ws/2/artist?query=${artistName}&fmt=json&limit=25`)
+    .then(res => res.json())
+    .then(json => json.artists.map(artist => ({  
+      id: artist.id,
+      name: artist.name
+    })));
+};
