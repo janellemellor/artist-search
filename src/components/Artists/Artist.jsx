fa85017f2cc5da7e08635316f13e27a562a620d7
janelle mellor <janellemellor@gmail.com>
2020-05-04 17:54:36 -0700

    add Artist item

---
+++ src/components/Artists/Artist.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+const Artist = ({ name }) => (
+  <>
+    <p>{name}</p>
+  </>
+
+);
+
+Artist.propTypes = {
+  name: PropTypes.string.isRequired
+};
+
+export default Artist;
+
