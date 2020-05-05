ee487b1b3dad5a8f846c34a580b3db7c4bfe2913
janelle mellor <janellemellor@gmail.com>
2020-05-04 19:00:32 -0700

    add Router to App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,17 @@
 import React from 'react';
+import {
+  Route, 
+  Switch, 
+  BrowserRouter as Router,
+} from 'react-router-dom';
 import ArtistSearch from '../../containers/ArtistSearch/ArtistSearch.jsx';
 
 export default function App() {
-  return < ArtistSearch />;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path ="/" component={ArtistSearch} />
+      </Switch>
+    </Router>
+  );
 }
