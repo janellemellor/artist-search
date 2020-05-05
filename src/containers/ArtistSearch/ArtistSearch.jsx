3122a8c5a824496b34d1cd3b9e7a4a415d9fb8f1
janelle mellor <janellemellor@gmail.com>
2020-05-04 19:56:16 -0700

    set up paging on artist search

--- src/containers/ArtistSearch/ArtistSearch.jsx
+++ src/containers/ArtistSearch/ArtistSearch.jsx
@@ -7,18 +7,23 @@ import { fetchArtists } from '../../services/getArtistData.js';
 const ArtistSearch = () => {
   const [artistNameInput, setArtistNameInput] = useState('');
   const [artistResults, setArtistResults] = useState([]);
+  const [page, setPage] = useState(1);
 
   const handleChange = ({ target }) => setArtistNameInput(target.value);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    fetchArtists(artistNameInput)
+    fetchArtists(artistNameInput, page)
       .then(res => setArtistResults(res));
   }; 
+
+  const handlePageChange = by => setPage(prevPage => prevPage + by);
 
    
   return (
     <>
+      <button onClick={() => handlePageChange(-1)} disabled={page === 1}>&lt;</button>
+      <button onClick={() => handlePageChange(1)} disabled={artistResults.length < 15}>&gt;</button>
       <Search onSubmit={handleSubmit} onChange={handleChange} searchInput={artistNameInput} />
       <Artists artistResults={artistResults} />
     </>
