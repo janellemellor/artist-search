f59944dfea04ac4f2f05bb67884a980a04c516dc
janelle mellor <janellemellor@gmail.com>
2020-05-04 20:27:09 -0700

    paging is kind of working

--- src/containers/ArtistSearch/ArtistSearch.jsx
+++ src/containers/ArtistSearch/ArtistSearch.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Search from '../../components/Search/Search.jsx';
 import Artists from '../../components/Artists/Artists.jsx';
@@ -7,7 +7,7 @@ import { fetchArtists } from '../../services/getArtistData.js';
 const ArtistSearch = () => {
   const [artistNameInput, setArtistNameInput] = useState('');
   const [artistResults, setArtistResults] = useState([]);
-  const [page, setPage] = useState(1);
+  const [page, setPage] = useState(0);
 
   const handleChange = ({ target }) => setArtistNameInput(target.value);
 
@@ -17,13 +17,20 @@ const ArtistSearch = () => {
       .then(res => setArtistResults(res));
   }; 
 
-  const handlePageChange = by => setPage(prevPage => prevPage + by);
+  const handlePageChange = (by) => { setPage(prevPage => prevPage + by);
+  };
 
+  useEffect(() => {
+    if(page > 0) {
+      fetchArtists(artistNameInput, page)
+        .then(res => setArtistResults(res));
+    }
+  }, [page]);
    
   return (
     <>
-      <button onClick={() => handlePageChange(-1)} disabled={page === 1}>&lt;</button>
-      <button onClick={() => handlePageChange(1)} disabled={artistResults.length < 15}>&gt;</button>
+      <button onClick={() => handlePageChange(-25)} disabled={page === 25}>&lt;</button>
+      <button onClick={() => handlePageChange(1)} disabled={artistResults.length < 25}>&gt;</button>
       <Search onSubmit={handleSubmit} onChange={handleChange} searchInput={artistNameInput} />
       <Artists artistResults={artistResults} />
     </>
