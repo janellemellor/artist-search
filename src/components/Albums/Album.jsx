22b28847218dbb3a7293d1785038c88174626027
janelle mellor <janellemellor@gmail.com>
2020-05-05 16:06:50 -0700

    add album item

--- src/components/Albums/Album.jsx
+++ src/components/Albums/Album.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
-const Album = ({title,}) => (
+const Album = ({ title, id, coverArt }) => (
   <section>
-    <h3>{title}</h3>
-    <
+    <Link to={`/${title}/${id}`} >
+      <h3>{title}</h3>
+      <img src={coverArt} alt={title} />
+    </Link>
   </section>
 );
 
