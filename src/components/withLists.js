8b15b1991c9e27db5124f2589851ee34c9f487d1
janelle mellor <janellemellor@gmail.com>
2020-05-04 18:56:00 -0700

    add link to withList

--- src/components/withLists.js
+++ src/components/withLists.js
@@ -1,10 +1,13 @@
 import React from 'react';
+// import { Link } from 'react-router-dom';
 
 const withList = (ListItemComponent, propName = 'list') => {
   function CreateListComponent(props) {
     const listItems = props[propName].map(item => (
       <li key={item.id}>
+        {/* <Link to={`/${item.id}`} > */}
         <ListItemComponent {...item} />
+        {/* </Link>   */}
       </li>    
     ));
 
