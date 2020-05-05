9ee0527768fda336c5d79dbead0ffc3253c02faa
janelle mellor <janellemellor@gmail.com>
2020-05-05 12:12:45 -0700

    remove link from withList

--- src/components/withLists.js
+++ src/components/withLists.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 const withList = (ListItemComponent, propName = 'list') => {
   function CreateListComponent(props) {
     const listItems = props[propName].map(item => (
       <li key={item.id}>
-        <Link to={`/${item.id}`} > 
-          <ListItemComponent {...item} />
-        </Link>  
+        <ListItemComponent {...item} />
       </li>    
     ));
 
