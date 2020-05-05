3ffd8d6d93adfe738cf367040f051fdfa2b90477
janelle mellor <janellemellor@gmail.com>
2020-05-05 12:19:20 -0700

    add Link to Artist

--- src/components/Artists/Artist.jsx
+++ src/components/Artists/Artist.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
-const Artist = ({ name }) => (
-  <>
+const Artist = ({ name, id }) => (
+  <Link to={`/${name}/${id}`} >
     <p>{name}</p>
-  </>
+  </Link>
 
 );
 
 Artist.propTypes = {
-  name: PropTypes.string.isRequired
+  name: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired
 };
 
 export default Artist;
