02f23327eda0f5fe06f02eeb2c90f720d8ab53d5
janelle mellor <janellemellor@gmail.com>
2020-05-04 22:30:35 -0700

    update props in form

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export const Search = ({ onSubmit, onChange }) => (
+export const Search = ({ onSubmit, onChange, searchInput }) => (
   <>
     <form onSubmit={onSubmit}>
       <fieldset>
-        <input type="text" name="artist-search" value="name" onChange={onChange}  />
+        <input type="text" name={searchInput} value={searchInput} onChange={onChange}  />
         <button>Find Artist!</button>
       </fieldset>
     </form>
@@ -15,6 +15,7 @@ export const Search = ({ onSubmit, onChange }) => (
 Search.propTypes = {
   onSubmit: PropTypes.func.isRequired, 
   onChange: PropTypes.func.isRequired, 
+  searchInput: PropTypes.string.isRequired
 };
 
 export default Search;
