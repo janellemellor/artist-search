2f5a80ea29b6a04af99c530d2917a4a6ca79aff4
janelle mellor <janellemellor@gmail.com>
2020-05-04 19:02:47 -0700

    update withList with working Link

--- src/components/withLists.js
+++ src/components/withLists.js
@@ -1,13 +1,13 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const withList = (ListItemComponent, propName = 'list') => {
   function CreateListComponent(props) {
     const listItems = props[propName].map(item => (
       <li key={item.id}>
-        {/* <Link to={`/${item.id}`} > */}
-        <ListItemComponent {...item} />
-        {/* </Link>   */}
+        <Link to={`/${item.id}`} > 
+          <ListItemComponent {...item} />
+        </Link>  
       </li>    
     ));
 
